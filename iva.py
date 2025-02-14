714d1722ff9625bf842e895d3dc8bc0bfd14e509
KarenDC5 <karen.duran@iteso.mx>
2025-02-14 08:47:27 -0600

    primer intento

--- iva.py
+++ iva.py
@@ -1,15 +1,23 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Se ingresa una cantidad sin iva y se regresa el total de la cantidad con iva
+Karen Durán
+757136
+Ingeniería Civil
+Algoritmos y Programación 
+ESI232B2
+14/02/25
+5 minutos 
 """
 
 # Declaraciones
-CONSTANTE = valor
+tasa_iva = 0.16
 
 # Entradas
-entrada = input()
+cantidad = float(input("Ingrese la cantidad sin IVA:"))
 
 # Proceso
-
+iva = cantidad * tasa_iva
+total = cantidad + iva
 
 # Salidas
-print(salida)
+print("Total", total)
